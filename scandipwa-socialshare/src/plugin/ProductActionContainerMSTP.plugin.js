e40bc0ece5e114f52c32e22f5605402f5d760fb5
GAkim <akim.goncar@gmail.com>
2021-05-31 11:03:36 +0300

    STEP-9 ProductAction Plugins

---
+++ scandipwa-socialshare/src/plugin/ProductActionContainerMSTP.plugin.js
@@ -0,0 +1,14 @@
+const mapStateToProps = (args, callback, instance) => {
+    const [state] = args;
+
+    return {
+        ...callback(...args),
+        socialShare: state.SocialShareReducer.socialShare
+    };
+};
+
+export default {
+    'Component/ProductActions/Container/mapStateToProps': {
+        function: mapStateToProps
+    }
+};
